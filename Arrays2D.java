9759784e3c857ae0b5f5e36446b6f5465d31e688
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-05-02 15:10:01 +0530

    2May

--- Arrays2D.java
+++ Arrays2D.java
@@ -19,7 +19,35 @@ public class Arrays2D {
                         {5,6,7,8},
                         {9,10,11,12},
                         {13,14,15,16}};
-        printSpiralMatrix(matrix);
+        //printSpiralMatrix(matrix);
+        diagonalSum(matrix);
+    }
+
+    public static void diagonalSum(int[][] matrix){
+        int sum = 0;
+        //brute force approach
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(i==j){
+        //             sum += matrix[i][j];
+        //         }else if((i+j) == matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+
+        //Optimized Approach
+        for(int i=0;i<matrix.length;i++){
+            sum += matrix[i][i];
+            //i+j = matrix.length-1
+            //j = matrix.length-1-i;
+            if(i!=matrix.length-1-i){
+                sum += matrix[i][matrix.length-1-i];
+            }
+        }
+
+        System.out.print("Sum of Diagonals : "+sum);
+
     }
 
     public static void printSpiralMatrix(int [][]matrix){
