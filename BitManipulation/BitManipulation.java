33a955eaaf4a8f56b7c7ad545e5605b5bd5dbfdb
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-05-05 22:47:46 +0530

    10PM5May

---
+++ BitManipulation/BitManipulation.java
@@ -0,0 +1,19 @@
+package BitManipulation;
+
+public class BitManipulation {
+    public static void main(String[] args) {
+        //Binary AND &
+        System.out.println(5 & 6);
+        //Binary OR |
+        System.out.println(5 | 6);
+        //Binary XOR ^
+        System.out.println(5 ^ 6);
+        //Binary One's Complement
+        System.out.println(~5);
+        System.out.println(~0);
+        //Binary Left Shift
+        System.out.println(5 << 2);//a<<b = a * 2^b
+        //Binary Right Shift
+        System.out.println(16 >> 2);//a>>b = a / 
+    }
+}
