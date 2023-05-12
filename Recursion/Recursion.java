3eaf401411ec0335ab536ff6cadf2b0f11df4e6b
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-05-13 01:19:01 +0530

    RecursionFirst

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,14 +1,47 @@
 package Recursion;
+
+import javax.swing.text.html.StyleSheet;
+
 class Recursion{
     public static void main(String[] args) {
         int n = 8;
-        int arr[] = {1,12,3,9,17,25,33,41};
+        // int arr[] = {1,12,3,9,17,25,33,41};
+        int arr[] = {8,3,6,9,5,10,2,5,3};
         //printDec(n);
         //printInc(n);
         //System.out.println(printFact(n));
         //System.out.println(sumOfN(n));
         //System.out.println("Nth Fibonacci term is : "+fib(n));
-        System.out.println("Given Array is Sorted ? : "+isSorted(arr,0));
+        //System.out.println("Given Array is Sorted ? : "+isSorted(arr,0));
+        //System.out.println(firstOccurance(arr, 5, 0));
+        System.out.println(lastOccurance(arr, 10, 0));
+    }
+
+    public static int lastOccurance(int[] arr, int key, int i){
+        //base case
+        if(i==arr.length-1){
+            return -1;
+        }
+        int isFound = lastOccurance(arr, key, i+1);
+        if(isFound != -1){
+            return isFound;
+        }
+        if(arr[i]==key){
+            return i;
+        }
+        return isFound;
+    }
+
+    public static int firstOccurance(int[] arr, int key, int i){
+        //base case
+        if(i==arr.length-1){
+            return -1;
+        }
+        if(arr[i] == key){
+            return i;
+        }
+        return firstOccurance(arr, key, i+1);
+
     }
 
     public static boolean isSorted(int[] arr, int i){
