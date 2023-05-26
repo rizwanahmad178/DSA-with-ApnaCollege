68b9ce8d802ac028978dd18cd3bdd523ab3bcce1
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-05-26 14:31:21 +0530

    nQueens

--- BackTracking/BackTracking.java
+++ BackTracking/BackTracking.java
@@ -1,14 +1,103 @@
 package BackTracking;
 
 public class BackTracking {
+
+    static int count = 0;
     public static void main(String[] args) {
         // int arr[] = new int[5];
         // changeArr(arr,0,1);
         // printArr(arr);
 
         //find subset and print them
-        String str = "abc";
-        findSubset(str, "", 0);
+        // String str = "abc";
+        // findSubset(str, "", 0);
+
+        //find permutations
+        // String str = "abcd";
+        // findPermutations(str, "");
+        
+        //nQueens Problem
+        int n = 4;
+        char board[][] = new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j] = 'x';
+            }
+        }
+        if(nQueens(board, 0)){
+            System.out.println("Solution is Possible::");
+            printBoard(board);
+        }else{
+            System.out.println("Solution is not Possibl::");
+        }
+        //System.out.println("Total ways to solve nQueens : "+count);
+    }
+
+    // public static void nQueens(char[][] board, int row){
+        public static boolean nQueens(char[][] board, int row){
+        //base case
+        if(row==board.length){
+            //printBoard(board);
+            //count++;
+            //return;
+            return true;
+        }
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board, row, j)==1){
+                board[row][j] = 'Q';
+                if(nQueens(board, row+1)){
+                    return true;
+                }
+                board[row][j] = 'x';
+            }
+        }
+        return false;
+    }
+
+    public static int isSafe(char[][] board, int row, int col){
+        //vertical check
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return 0;
+            }
+        }
+        //dia left check
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return 0;
+            }
+        }
+        //dia right check
+        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void printBoard(char[][] board){
+        System.out.println("New Board...");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void findPermutations(String str, String ans){
+        //base case
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        //recursion
+        for(int i=0;i<str.length();i++){
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            findPermutations(newStr, ans+curr);     
+        }
     }
 
     public static void findSubset(String str, String ans, int i){
