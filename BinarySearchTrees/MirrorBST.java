809dac3e1652594847311d789e17e1ee45c2c201
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-09-04 23:56:30 +0530

    MirrorBST

---
+++ BinarySearchTrees/MirrorBST.java
@@ -0,0 +1,50 @@
+package BinarySearchTrees;
+
+public class MirrorBST {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node createMirror(Node root){
+        if(root == null){
+            return null;
+        }
+        Node leftSubTree = createMirror(root.left);
+        Node rightSubTree = createMirror(root.right);
+
+        root.left = rightSubTree;
+        root.right = leftSubTree;
+
+        return root;
+    }
+
+    public static void preorder(Node root){
+        if(root==null){
+            return ;
+        }
+
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.right = new Node(10);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.right.right = new Node(11);
+
+        root = createMirror(root);
+        preorder(root);
+
+    }
+}
