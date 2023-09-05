ce6b635a33e1b89baa50b438ce324a2e251aefd6
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-09-05 18:47:10 +0530

    BBST

--- BinarySearchTrees/BSTClass.java
+++ BinarySearchTrees/BSTClass.java
@@ -1,5 +1,7 @@
 package BinarySearchTrees;
 
+import java.util.ArrayList;
+
 public class BSTClass {
     static class Node{
         int data;
@@ -34,10 +36,48 @@ public class BSTClass {
         preorder(root.left);
         preorder(root.right);
     }
-    public static void main(String[] args) {
-        int arr[] = {3,5,6,8,10,11,12};
-        Node root = createBST(arr,0,arr.length-1);
-        preorder(root);
 
+    public static void getInorder(Node root, ArrayList<Integer> inorder){
+        if(root==null)
+            return;
+        getInorder(root.left, inorder);
+        inorder.add(root.data);
+        getInorder(root.right, inorder);
+    }
+    
+    public static Node createBalancedBST(ArrayList<Integer> inorder, int st, int end){
+        if(st>end){
+            return null;
+        }
+        int mid = (st+end)/2;
+        Node root = new Node(inorder.get(mid));
+        root.left = createBalancedBST(inorder, st, mid-1);
+        root.right = createBalancedBST(inorder, mid+1, end);
+        return root;
+    }
+
+    public static Node convertBST(Node root){
+        ArrayList<Integer> inorder = new ArrayList<>();
+        getInorder(root, inorder);
+        root = createBalancedBST(inorder, 0, inorder.size()-1);
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+        // int arr[] = {3,5,6,8,10,11,12};
+        // Node root = createBST(arr,0,arr.length-1);
+        // preorder(root);
+        Node root = new Node(8);
+        root.left = new Node(6);
+        root.left.left = new Node(5);
+        root.left.left.left = new Node(3);
+        
+        root.right = new Node(10);
+        root.right.right = new Node(11);
+        root.right.right.right = new Node(12);
+
+        root = convertBST(root);
+        preorder(root);
     }
 }
