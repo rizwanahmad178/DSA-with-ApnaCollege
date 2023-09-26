9e64641cd984138651c63564c969f824295c216d
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-09-26 19:41:41 +0530

    DP2

--- DynamicPro/DPClass.java
+++ DynamicPro/DPClass.java
@@ -11,9 +11,19 @@ public class DPClass {
         f[n] = fib(n-1,f) + fib(n-2,f);
         return f[n];
     }
+    public static int tabulation(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
         int f[] = new int[n+1];// 0 0 0 0 0 0
-        System.out.println(fib(n,f));
+        //System.out.println(fib(n,f));
+        System.out.println(tabulation(n));
     }
 }
