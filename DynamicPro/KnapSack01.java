4a0ac7680f1a3c91d3d3ce1d8e34264180b61dd5
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-09-28 16:57:02 +0530

    DP3

--- DynamicPro/KnapSack01.java
+++ DynamicPro/KnapSack01.java
@@ -34,6 +34,31 @@ public class KnapSack01 {
             return dp[n][W];
         }
     }
+    public static int knapSackTabulation(int[] val, int[] wt, int W){
+        int n = val.length;
+        int dp[][] = new int[n+1][W+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0] = 0;
+        }
+        for(int i=0;i<dp[0].length;i++){
+            dp[0][i] = 0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                int v = val[i-1];
+                int w = wt[i-1];
+                if(w<=j){
+                    int incProfit = v + dp[i-1][j-w];
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                }else{
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+        return dp[n][W];
+    }
     public static void main(String[] args) {
         int val[] = {15,14,10,45,30};
         int wt[] = {2,5,1,3,4};
@@ -46,5 +71,6 @@ public class KnapSack01 {
             }
         }
         System.out.println("Max Profit : "+knapSackMemoization(val,wt,W,val.length, dp));
+        System.out.println("Max Profit : "+knapSackTabulation(val,wt,W));
     }
 }
