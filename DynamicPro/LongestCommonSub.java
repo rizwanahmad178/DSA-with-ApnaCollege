cc627c61feb3a450e89a0ae532dc59d09d5ba318
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-09-30 02:10:15 +0530

    LCST

--- DynamicPro/LongestCommonSub.java
+++ DynamicPro/LongestCommonSub.java
@@ -28,6 +28,32 @@ public class LongestCommonSub {
             return dp[n][m] = Math.max(ans1, ans2);
         }
     }
+    public static int lcsTabulation(String str1, String str2){
+        int n = str1.length();
+        int m = str2.length();
+        String str = "";
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1,ans2);
+                }
+            }
+        }
+        System.out.println("Longest Common Subsequence : "+str);
+        return dp[n][m];
+    }
     public static void main(String[] args) {
         String str1 = "abcdge";
         String str2 = "abedg";
@@ -41,5 +67,6 @@ public class LongestCommonSub {
             }
         }
         System.out.println("LongestCommonSubsequence : "+lcsMemoization(str1, str2, n, m, dp));
+        System.out.println("LongestCommonSubsequence : "+lcsTabulation(str1, str2));
     }
 }
