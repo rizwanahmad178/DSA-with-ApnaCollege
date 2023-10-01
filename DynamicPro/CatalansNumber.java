e6560b888a5aede61160fe09843ead351b8b9b63
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-10-02 01:58:10 +0530

    CatalansTab

--- DynamicPro/CatalansNumber.java
+++ DynamicPro/CatalansNumber.java
@@ -26,11 +26,23 @@ public class CatalansNumber {
         }
         return dp[n] = ans;
     }
+    public static int catalansTab(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 4;
-        System.out.println("Catalan's Value of number "+n+" : " +catalansNumberWithRecursion(n));
+        System.out.println("Catalan's Value of number "+n+" (Using Recursion): " +catalansNumberWithRecursion(n));
         int dp[] = new int[n+1];
         Arrays.fill(dp, -1);
-        System.out.println("Catalan's Value of number "+n+" : " +catalansMem(n,dp));
+        System.out.println("Catalan's Value of number "+n+" (Using Memoization): " +catalansMem(n,dp));
+        System.out.println("Catalan's Value of number "+n+" (Using Tabulation): " +catalansTab(n));
     }
 }
