a2f993848328ba6d1d3f9872d186f605f1eee7e0
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-10-03 23:09:55 +0530

    MCMMem

---
+++ DynamicPro/MatrixChainM.java
@@ -0,0 +1,51 @@
+package DynamicPro;
+
+import java.util.Arrays;
+
+public class MatrixChainM {
+    public static int mcm(int[] arr, int i, int j){
+        if(i==j){
+            return 0;
+        }
+        int minCost = Integer.MAX_VALUE;
+        for(int k=i;k<=j-1;k++){
+            int cost1 = mcm(arr, i, k);
+            int cost2 = mcm(arr, k+1, j);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            minCost = Math.min(minCost, finalCost);
+        }
+     
+        return minCost;
+    }
+    public static int mcmMem(int[] arr, int i, int j, int[][] dp){
+        if(i==j){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+
+        int minCost = Integer.MAX_VALUE;
+        for(int k=i;k<=j-1;k++){
+            int cost1 = mcmMem(arr, i, k, dp);
+            int cost2 = mcmMem(arr, k+1, j, dp);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            minCost = Math.min(minCost, finalCost);
+        }
+     
+        return minCost;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,3};
+        int n = arr.length;
+        System.out.println("Matrix Chain Multiplication (Recursion) : "+mcm(arr,1,n-1));
+        int dp[][] = new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        System.out.println("Matrix Chain Multiplication (Memoization) : "+mcmMem(arr,1,n-1, dp));
+    }
+}
