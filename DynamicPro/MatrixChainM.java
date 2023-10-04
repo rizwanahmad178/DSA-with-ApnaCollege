06c72c87eed2d57f31370f0e2d2543c34978d3ca
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-10-04 11:36:39 +0530

    MCMTab

--- DynamicPro/MatrixChainM.java
+++ DynamicPro/MatrixChainM.java
@@ -38,6 +38,29 @@ public class MatrixChainM {
      
         return minCost;
     }
+    public static int mcmTab(int[] arr){
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j] = 0;
+            }
+        }
+
+        for(int len = 2; len <= n-1; len++){
+            for(int i=1;i<=n-len;i++){
+                int j = i + len -1;
+                dp[i][j] = Integer.MAX_VALUE; 
+                for(int k = i; k <= j-1; k++){
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1+cost2+cost3);
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,3};
         int n = arr.length;
@@ -47,5 +70,6 @@ public class MatrixChainM {
             Arrays.fill(dp[i], -1);
         }
         System.out.println("Matrix Chain Multiplication (Memoization) : "+mcmMem(arr,1,n-1, dp));
+        System.out.println("Matrix Chain Multiplication (Tabulation) : "+mcmTab(arr));
     }
 }
