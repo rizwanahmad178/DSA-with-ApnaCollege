ab73b90064fdf5cf0654a89a919d606693e77981
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2023-10-06 00:05:32 +0530

    UpdateST

--- SegmentTrees/SegmentTree.java
+++ SegmentTrees/SegmentTree.java
@@ -35,6 +35,26 @@ public class SegmentTree {
         int n = arr.length;
         return getSumUtil(0, 0, n-1, qi, qj);
     }
+
+    public static void updateUtil(int i, int si, int sj, int idx, int diff){
+        if(idx > sj || idx < si){
+            return;
+        }
+
+        tree[i] += diff;
+        if(si != sj){
+            int mid = (si + sj)/2;
+            updateUtil(2*i+1, si, mid, idx, diff);
+            updateUtil(2*i+2, mid+1, sj, idx, diff);
+        }
+    }
+    public static void update(int arr[], int idx, int newVal){
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+
+        updateUtil(0, 0, n-1, idx, diff);
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
         init(arr.length);
@@ -43,5 +63,7 @@ public class SegmentTree {
             System.out.print(tree[i] + " ");
         }
         System.out.println("\nSum of the given range for Segment Tree : "+getSum(arr, 2, 5));
+        update(arr, 2, 2);
+        System.out.println("Sum of the given range for Segment Tree : "+getSum(arr, 2, 5));
     }
 }
